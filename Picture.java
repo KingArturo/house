feef445368054294d39483879b3c5f04cd961a1e
Arturo <arturoclase@gmail.com>
2019-10-10 13:13:12 +0200

    Ahora tiene cesped y el sol se mueve hacai arriba

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Square campo;
 
     /**
      * Constructor for objects of class Picture
@@ -48,12 +49,24 @@ public class Picture
         roof.moveVertical(-60);
         roof.makeVisible();
 
+        
+        
+        
+        campo = new Square();
+        campo.changeColor("green");
+        campo.changeSize(80);
+        campo.makeVisible();
+        campo.changeSize(5000);
+        campo.moveHorizontal(-1087);
+        campo.moveVertical(138);
+        
         sun = new Circle();
         sun.changeColor("yellow");
         sun.moveHorizontal(100);
-        sun.moveVertical(-40);
+        sun.moveVertical(100);
         sun.changeSize(80);
         sun.makeVisible();
+        sun.slowMoveVertical(-120);
     }
 
     /**
